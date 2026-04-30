c565879e12c39d2d8f4d408cc167d369feb4d902
JOOXX <citadelproductsofficial@gmail.com>
2026-04-30 21:20:43 +0530

    Add PWA support for mobile installation

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'finance-manager-v1';
+const ASSETS = [
+  'index.html',
+  'style.css',
+  'app.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
